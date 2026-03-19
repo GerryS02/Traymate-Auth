2b2dbfd50040c604ef531af276fb526e15a58e68
Adam <aloepker@gmail.com>
2026-03-18 20:12:44 -0700

    updated  import

--- backend/src/main/java/com/traymate/backend/mealOrders/MealOrdersRepository.java
+++ backend/src/main/java/com/traymate/backend/mealOrders/MealOrdersRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface MealOrdersRepository extends JpaRepository<MealOrders, Integer> {
 
