79109b39800169a1834adba9f550d7145db8f951
Gerry Summers <Singur95878@student.edmonds.edu>
2026-05-09 21:28:51 -0700

    testing manxi's setavailability code

--- backend/src/main/java/com/traymate/backend/menu/MenuController.java
+++ backend/src/main/java/com/traymate/backend/menu/MenuController.java
@@ -1,44 +1,3 @@
-// package com.traymate.backend.menu;
-
-// import lombok.RequiredArgsConstructor;
-
-// import org.springframework.web.bind.annotation.*;
-
-// import java.util.List;
-
-// @RestController
-// @RequestMapping("/menu")
-// @RequiredArgsConstructor
-// public class MenuController {
-    
-//     private final MenuService menuService;
-
-//     @GetMapping
-//     public List<Meal> getAllMeals(){
-//         return menuService.getAllMeals();
-//     }
-
-//     @GetMapping("/available")
-//     public List<Meal> getAvailableMeals(){
-//         return menuService.getAvailableMeals();
-//     }
-
-//     @GetMapping("/period/{mealperiod}")
-//     public List<Meal> getMealsByPeriod(@PathVariable String mealperiod) {
-//         return menuService.getMealsByPeriod(mealperiod);
-//     }
-
-//     @GetMapping("/period/drinks")
-//     public List<Meal> getDrinks() {
-//         return menuService.getDrinks();
-//     }
-
-//     @GetMapping("/period/sides")
-//     public List<Meal> getSides() {
-//         return menuService.getSides();  
-//     }
-// }
-
 package com.traymate.backend.menu;
 
 import lombok.RequiredArgsConstructor;
@@ -88,7 +47,7 @@ public class MenuController {
      * admin-only /admin/** prefix. Body: { "available": true|false }
      */
     @PatchMapping("/{id}/availability")
-    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_KITCHEN_STAFF','ROLE_KITCHEN')")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN', 'ROLE_KITCHEN')")
     public Meal setAvailability(
             @PathVariable Integer id,
             @RequestBody Map<String, Boolean> body) {
